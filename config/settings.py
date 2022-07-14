dee7d1c2df8085f3f37b0bba3cdd6e5f1662a01d
Potagashev <alexpotagashev@gmail.com>
2022-07-15 05:00:55 +0700

    almost mvp

--- config/settings.py
+++ config/settings.py
@@ -258,6 +258,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
