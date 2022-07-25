ced85a7bdf8e1c6a32e8bfd0325eec034c9cf334
Potagashev <alexpotagashev@gmail.com>
2022-07-25 16:44:17 +0700

    added filters

--- filter/views.py
+++ filter/views.py
@@ -8,12 +8,13 @@ from project.serializers import ProjectSerializer, ProjectSerializerWithoutDescr
 
 
 class FilterProjectsAPIView(generics.ListAPIView):
-    """filtering can be priority, name, start_time, end_time, order, type, direction_type"""
+    """filtering can be priority, name, start_time, end_time, order, type, direction_type
+    REQUIRED REQUEST FORMAT: filter_projects/?filter_by=<field>
+    """
     serializer_class = ProjectSerializerWithoutDescription
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """there can be priority, name, start_time, end_time, order, type, direction_type"""
         param = self.request.query_params.get('filter_by')
         if param:
             return Project.objects.filter(users__id=self.request.user.id).order_by(param)
