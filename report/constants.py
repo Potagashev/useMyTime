7f12b4f0aa33f65dda0bffa4c7f7440b337a7de1
Potagashev <alexpotagashev@gmail.com>
2022-09-17 02:54:25 +0700

    reports draft

--- report/constants.py
+++ report/constants.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
 
 
-def you_didnt_provide_all_data_response(data: tuple):
+def handle_required_fileds_were_not_provided(data: tuple):
     return Response(status=400, data={'details': f'You did not provide all data {data}'})
--- report/views.py
+++ report/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from datetime import datetime
 
-from report.constants import you_didnt_provide_all_data_response
+from report.constants import handle_required_fileds_were_not_provided
+from timer.models import TaskTimer
 
 
 class GetReportForPeriod(APIView):
@@ -13,24 +14,58 @@ class GetReportForPeriod(APIView):
             start = self.request.data['start']
             end = self.request.data['end']
         except KeyError as err:
-            return you_didnt_provide_all_data_response(err.args)
+            return handle_required_fileds_were_not_provided(err.args)
 
         start_date = datetime.strptime(start, '%Y-%m-%d').date()
         end_date = datetime.strptime(end, '%Y-%m-%d').date()
-        print((end_date - start_date).days)
 
-        response = Response()
-        return response
-        # for i in range(30):
-        #     row = {}
-        #     row['Отдел'] = user.department
-        #     row['Сотрудник'] = user.displayName
-        #     row['Часы'] = get_total_time_by task(task)
-        #     hours += row['Часы']
+        sessions = TaskTimer.objects.filter(start_time__gte=start, end_time__lte=end, task__assignee=user)
+
+        total_time = sessions.latest('end_time').end_time - sessions[0].start_time
+
+        report = {}
+        for session in sessions:
+            task = session.task
+
+            if task.id in report:
+                hours = (session.end_time - session.start_time).seconds / 3600
+                percents = 100 * hours / (total_time.seconds / 3600)
+                report[task.id]['hours'] += hours
+                report[task.id]['percents'] += percents
+
+            else:
+                department = task.assignee.department
+                employee = task.assignee.displayName
+                hours = (session.end_time - session.start_time).seconds / 3600
+                percents = 100 * hours / (total_time.seconds / 3600)
+                direction_type = task.project.direction_type
+                project_type = task.project.type
+                order = task.project.order
+                description = task.description
+
+                row = {
+                    "department": department,
+                    "employee": employee,
+                    "hours": hours,
+                    "percents": percents,
+                    "direction_type": direction_type,
+                    "project_type": project_type,
+                    "order": order,
+                    "description": description
+                }
+                report[task.id] = row
+            print("alright im working on it")
+
+        return Response(data=report, status=200)
+        # department
+        # FIO
+        # hours
+        # percents
+        # Б
+        # Type
+        # Order
+        # Description
+        # Hours sum
+        # Sum %
         #     row['%'] = 100 * (row['Часы'] / get_total_time_for_period())
-        #     row['Б'] = task.project.direction_type
-        #     row['Тип'] = task.project.type
-        #     row['Заказ'] = task.project.order
-        #     row['Расшифровка'] = task.description
-        #
         # total_time = get_total_time_for_period(start, end)
